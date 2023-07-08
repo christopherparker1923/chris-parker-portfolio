6004f0a7f81d05da53da4414b38623631a824739
christopherparker1923 <christmas1923@gmail.com>
2023-07-07 21:18:24 -0400

    Email Service

--- src/components/Contact/EmailMe.js
+++ src/components/Contact/EmailMe.js
@@ -10,10 +10,10 @@ function Contact() {
     // Send an email using EmailJS
     emailjs
       .sendForm(
-        "YOUR_SERVICE_ID",
-        "YOUR_TEMPLATE_ID",
+        "service_huu9tne",
+        "template_573vg41",
         form.current,
-        "YOUR_PUBLIC_KEY"
+        "8RTMXyLFwVP1DbVUp"
       )
       .then(
         (result) => {
@@ -28,81 +28,73 @@ function Contact() {
   return (
     <Container fluid className="contact-section contact-left contact-right">
       <Particle />
-      <Row>
-        <Col md={8} className="home-about-description">
-          <h1 style={{ fontSize: "2.6em" }}>
-            I'D LOVE TO HEAR <span className="purple"> FROM YOU </span>
-          </h1>
-          <p className="home-about-body">
-            I fell in love with programming during my undergrad in Mechatronics.
-            <br />
-            <br />I am most proficient in
-            <i>
-              <b className="purple"> Python, and TypeScript, </b>
-            </i>
-            having used these professionaly since graduation.
-            <br />
-            <br />I am also familiar with other languages from my studies and
-            hobbying &nbsp;
-            <br />
-            <br />I am currently seeking a full time position, my best
-            attributes have always been my
-            <i>
-              <b className="purple"> dedication and quick learning</b>
-            </i>
-          </p>
-        </Col>
-      </Row>
-      <Row className="contact">
-        <Form onSubmit={(e) => handleSubmit(e)} ref={form}>
-          <Form.Group controlId="formName">
-            <Form.Control
-              type="text"
-              name="user_name"
-              placeholder="Enter your name"
-              required
-            />
-            <Form.Label className="floating-label">Name</Form.Label>{" "}
-          </Form.Group>
+      <Container>
+        <Row>
+          <Col md={8} className="home-about-description">
+            <h1 style={{ fontSize: "2.6em" }}>
+              I'D LOVE TO HEAR <span className="purple"> FROM YOU </span>
+            </h1>
+            <p className="home-about-body">
+              You can find my contact info on my{" "}
+              <span className="purple"> resume </span> or feel free to use the
+              form below.
+              <br />
+              <br />I look forward to hearing from you about a job or just
+              connecting to grow my network.
+            </p>
+          </Col>
+        </Row>
+        <Row className="contact">
+          <Form onSubmit={(e) => handleSubmit(e)} ref={form}>
+            <Form.Group controlId="formName">
+              <Form.Control
+                type="text"
+                name="user_name"
+                placeholder="Enter your name"
+                required
+              />
+              <Form.Label className="floating-label">Name</Form.Label>{" "}
+            </Form.Group>
 
-          <Form.Group controlId="formEmail">
-            <Form.Control
-              type="email"
-              name="user_email"
-              placeholder="Enter your email"
-              required
-            />
-            <Form.Label className="floating-label">Email</Form.Label>{" "}
-          </Form.Group>
+            <Form.Group controlId="formEmail">
+              <Form.Control
+                type="email"
+                name="user_email"
+                placeholder="Enter your email"
+                required
+              />
+              <Form.Label className="floating-label">Email</Form.Label>{" "}
+            </Form.Group>
 
-          <Form.Group controlId="formMessage">
-            <Form.Control
-              as="textarea"
-              name="message"
-              rows={3}
-              placeholder="Your message"
-              required
-              style={{ resize: "none" }}
-            />
-            <Form.Label className="floating-label">Message</Form.Label>{" "}
-          </Form.Group>
+            <Form.Group controlId="formMessage">
+              <Form.Control
+                as="textarea"
+                name="message"
+                rows={3}
+                placeholder="Your message"
+                required
+                style={{ resize: "none" }}
+              />
+              <Form.Label className="floating-label">Message</Form.Label>{" "}
+            </Form.Group>
 
-          <Button
-            variant="primary"
-            type="submit"
-            style={{
-              marginTop: "2rem",
-              textTransform: "uppercase",
-              borderRadius: "0",
-              backgroundColor: "#444",
-              borderColor: "#333",
-              borderWidth: "0px",
-            }}
-          >
-            Send Message
-          </Button>
-        </Form>
-      </Row>
+            <Button
+              variant="primary"
+              type="submit"
+              style={{
+                marginTop: "2rem",
+                textTransform: "uppercase",
+                borderRadius: "0",
+                backgroundColor: "#444",
+                borderColor: "#333",
+                borderWidth: "0px",
+              }}
+            >
+              Send Message
+            </Button>
+          </Form>
+        </Row>
+      </Container>
     </Container>
   );
 }
--- src/components/Particle.js
+++ src/components/Particle.js
@@ -5,6 +5,7 @@ function Particle() {
   return (
     <Particles
       id="tsparticles"
+      className="overlay"
       params={{
         particles: {
           number: {
